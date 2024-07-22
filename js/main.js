49f06971b42240789b0b33faa9f86b2130f367ac
abdo00o00 <rahma.abdallah.eloo00o@gmail.com>
2024-07-22 21:38:10 +0300

    Fix some features

--- js/main.js
+++ js/main.js
@@ -1,65 +1,42 @@
-document.addEventListener('scroll', function() {
-    const circle = document.querySelector('.circle');
-    const scrollPercentage = window.scrollY / (document.body.scrollHeight - window.innerHeight);
-    const dashoffset = 150.796 * (1 - scrollPercentage);
-    circle.style.strokeDashoffset = dashoffset;
-});
+// document.addEventListener('scroll', function() {
+//     const circle = document.querySelector('.circle');
+//     const scrollPercentage = window.scrollY / (document.body.scrollHeight - window.innerHeight);
+//     const dashoffset = 150.796 * (1 - scrollPercentage);
+//     circle.style.strokeDashoffset = dashoffset;
+// });
  
 
 $(document).ready(function() {      
-    // $(document).mouseup(function(e) {
-    // for (let index = 0; index < 10; index+0) {
-        $(document).mousemove(function(e) {
-            $('#dot').css({
-            left: (e.clientX) + 'px',
-            top: (e.clientY) + 'px'
-            });
-        });
-        
+
+    // $(document).mousemove(function(e) {
+    //     $('#dot').css({
+    //     left: (e.clientX) + 'px',
+    //     top: (e.clientY) + 'px'
+    //     });
+    // });
+    // function toBigSmallNav() { 
+    //     if ($('body, html').outerWidth() < 1000) {
+            
+    //         $(".menu-extras").fadeIn(); // إظهار قائمة "menu-extras" بسلاسة
+    //         $("#navmenu-nav").fadeOut(); // إخفاء قائمة "navmenu-nav" بسلاسة
+    //         $("ul.appended-menu").fadeIn(); // إظهار القائمة المضافة بسلاسة
+            
+    //         // console.log("small nav");
+    //     } else {
+            
+    //         $(".menu-extras").fadeOut(); // إخفاء قائمة "menu-extras" بسلاسة
+    //         $("ul.appended-menu").fadeOut(); // إخفاء القائمة المضافة بسلاسة
+    //         $("#navmenu-nav").fadeIn(); // إظهار قائمة "navmenu-nav" بسلاسة
+            
+    //         // console.log("big nav");
+    //     }
+    //     // console.log("body width", $('body, html').outerWidth());
     // }
-    // })
-
-        // var dot = $('#dot');
-        // var mouseX = 0, mouseY = 0;
-
-        // $(document).mousemove(function(e) {
-        //     mouseX = e.pageX;
-        //     mouseY = e.pageY;
-        // });
-
-        // function moveDot() {
-        //     dot.css({
-        //         left: mouseX + 'px',
-        //         top: mouseY + 'px'
-        //     });
-        //     requestAnimationFrame(moveDot);
-        // }
-
-        // moveDot();
-      
-
-
+    // $(window).resize( function() {
+    //     toBigSmallNav();
+    // }    );
+    // toBigSmallNav();
+    
+    console.log("project done");
+        
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// $(".single-cat").mouseenter(function(){
-//     $(".CEOs .par-ul").stop(true, true).slideDown(300).;
-//     console.log("kk");
-// });
-
-// $(".single-cat").mouseleave(function() {
-//     $(".CEOs .par-ul").stop(true, true).slideUp(300);
-// });
-
